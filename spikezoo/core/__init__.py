e64dfc2da8eb618d93294512a656b89fb7054531
chenkang455 <455130517@qq.com>
2026-03-10 01:38:51 +0800

    feat(TASK-021): 设计模型注册机制，支持插件化扩展

--- spikezoo/core/__init__.py
+++ spikezoo/core/__init__.py
@@ -1,10 +1,13 @@
 from .task_manager import TaskManager, TaskRegistry
 from .plugin_base import PluginBase
 from .task_runner import TaskRunner
+from .model_registry import ModelRegistry, ModelInfo
 
 __all__ = [
     "TaskManager",
     "TaskRegistry",
     "PluginBase",
-    "TaskRunner"
+    "TaskRunner",
+    "ModelRegistry",
+    "ModelInfo"
 ]
